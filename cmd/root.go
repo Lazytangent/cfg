fa001bc5cbc8a7da992150aea15f51edf3c0f723
Lazytangent <peter.tn.mai@gmail.com>
2023-02-19 18:44:45 -0600

    Create basic CLI app

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"fmt"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "cfg",
+	Short: "A convenience wrapper for handling dotfiles with a bare git repository.",
+	Run: run,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(cmd *cobra.Command, args []string) {
+	fmt.Println("hello from cfg")
+}
