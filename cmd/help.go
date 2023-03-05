c927a9617aaa13ede9e4cc1f64f22d8b4b958802
Lazytangent <peter.tn.mai@gmail.com>
2023-03-04 18:35:00 -0600

    Add color to help text

---
+++ cmd/help.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/fatih/color"
+)
+
+func init() {
+	cobra.AddTemplateFunc("StyleHeading", color.New(color.FgGreen).SprintFunc())
+	usageTemplate := rootCmd.UsageTemplate()
+	usageTemplate = strings.NewReplacer(
+		`Usage:`, `{{StyleHeading "Usage:"}}`,
+		`Aliases:`, `{{StyleHeading "Aliases:"}}`,
+		`Available Commands:`, `{{StyleHeading "Available Commands:"}}`,
+		`Global Flags:`, `{{StyleHeading "Global Flags:"}}`,
+	).Replace(usageTemplate)
+
+	re := regexp.MustCompile(`(?m)^Flags:\s*$`)
+	usageTemplate = re.ReplaceAllLiteralString(usageTemplate, `{{StyleHeading "Flags:"}}`)
+	rootCmd.SetUsageTemplate(usageTemplate)
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.20
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/fatih/color v1.14.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 )
