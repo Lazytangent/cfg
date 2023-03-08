cbd4b188bb23d6f8b81c3143c731a9171a701486
Lazytangent <peter.tn.mai@gmail.com>
2023-03-08 09:35:13 -0600

    Begin to format output of `status` cmd

--- cmd/status/root.go
+++ cmd/status/root.go
@@ -3,11 +3,16 @@ package status
 import (
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/fatih/color"
 
 	"github.com/lazytangent/cfg/git"
 )
+
+const notStaged = `Changes not staged for commit:`
 
 func Run(cmd *cobra.Command, args []string) {
 	debug, err := cmd.Flags().GetBool("debug")
@@ -23,5 +28,23 @@ func Run(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	fmt.Print(output)
+	outputSplit := strings.Split(output, notStaged)
+	notStagedSection := outputSplit[1]
+	modifiedRe := regexp.MustCompile(`^\s+modified:\s+`)
+
+	modifiedSection := []string{}
+
+	for _, line := range strings.Split(notStagedSection, "\n") {
+		if modifiedRe.MatchString(line) {
+			split := modifiedRe.Split(line, -1)
+			modifiedSection = append(modifiedSection, split[1])
+		}
+	}
+
+	c := color.New(color.FgHiWhite).Add(color.Bold)
+	c.Println("Modified Files:")
+	for _, line := range modifiedSection {
+		newLine := fmt.Sprintf("\t%s", line)
+		color.Red(newLine)
+	}
 }
