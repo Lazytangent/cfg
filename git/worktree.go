c9bf261006d55527527667f0b04d98b770501cf5
Peter Mai <peter.tn.mai@gmail.com>
2023-08-23 17:26:19 -0500

    Use Open to create Repository object

--- git/worktree.go
+++ git/worktree.go
@@ -3,7 +3,10 @@ package git
 import (
 	"log"
 
+	"github.com/go-git/go-billy/v5/osfs"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/cache"
+	"github.com/go-git/go-git/v5/storage/filesystem"
 	"github.com/lazytangent/cfg/config"
 )
 
@@ -12,8 +15,13 @@ import (
 func Worktree() (*git.Worktree, error) {
 	cfg := config.Parse(config.ReadConfigFile())
 	gitDir := config.ParseTildeInPath(cfg.GitDir)
+	workTree := config.ParseTildeInPath(cfg.WorkTree)
 
-	repo, err := git.PlainOpen(gitDir)
+	dotGitFs := osfs.New(gitDir)
+	dotGitStorer := filesystem.NewStorage(dotGitFs, cache.NewObjectLRUDefault())
+	tree := osfs.New(workTree)
+
+	repo, err := git.Open(dotGitStorer, tree)
 	if err != nil {
 		log.Fatalf("git.PlainOpen: %s", err.Error())
 	}
