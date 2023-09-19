56cdf4f2ca2b39a5df1242951108f2f574b8f7b9
Peter Mai <peter.tn.mai@gmail.com>
2023-09-19 11:28:38 -0500

    Possible to open repository with libgit2, but most functionality doesn't work since it's a bare repo

--- cmd/test/test.go
+++ cmd/test/test.go
@@ -1,6 +1,14 @@
 package test
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+	git "github.com/libgit2/git2go/v34"
+
+	"github.com/lazytangent/cfg/config"
+)
 
 var Cmd = &cobra.Command{
 	Use: "test",
@@ -8,5 +16,22 @@ var Cmd = &cobra.Command{
 }
 
 func run(cmd *cobra.Command, args []string) {
+	cfg := config.Parse(config.ReadConfigFile())
+	repo, err := git.OpenRepository(config.ParseTildeInPath(cfg.GitDir))
+	if err != nil {
+		log.Fatalf("git.OpenRepository: %s", err)
+	}
 
+	statusOpt := &git.StatusOptions{}
+	statusList, err := repo.StatusList(statusOpt)
+	if err != nil {
+		log.Fatalf("repo.StatusList: %s", err)
+	}
+
+	entryCount, err := statusList.EntryCount()
+	if err != nil {
+		log.Fatalf("statusList.EntryCount: %s", err)
+	}
+
+	fmt.Printf("  %d entries in status list\n", entryCount)
 }
